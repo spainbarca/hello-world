36539d469af8d818a4eda798dfcbf26dc7607d69
Noah <spain.barcelona.1999@gmail.com>
2023-11-20 22:12:41 -0500

    Comentarios

---
+++ comentarios.py
@@ -0,0 +1,11 @@
+# Se usa numeral o signo gato para comentarios de linea.
+print('Gege')
+
+"""
+He gave a statement expressing his support to the people.
+Hizo una declaración expresando su apoyo a la gente. 
+2. (jurídico). a. la declaración (F).
+
+Se usa triple comilla doble para comentarios en bloque
+"""
+
