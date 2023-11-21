642370a82549b651c4b3d442ab13f1ac4081345c
Noah <spain.barcelona.1999@gmail.com>
2023-11-20 22:59:59 -0500

    Convertir tipos de datos

--- entrada.py
+++ entrada.py
@@ -1,5 +1,21 @@
 #Se usa la funcion input y devuelve un valor en string
 
+"""
 nombre = input("Coloque su nombre completo: ") #str
 print(nombre)
 print(type(nombre))
+"""
+
+#Enteros
+edad = int(input("Ingresa tu edad: "))
+print(edad)
+print(type(edad))
+
+#Flotante
+altura = float(input("Ingresa tu altura: "))
+print(altura)
+print(type(altura))
+
+#Booleano
+autoriza = input("Autorizas el programa?? (Si/No)") == 'si'
+print(autoriza)
