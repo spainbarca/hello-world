c462118733fc1a1fb9c740228925efa6a6768484
Noah <spain.barcelona.1999@gmail.com>
2023-11-21 18:44:14 -0500

    Indice de tuplas

---
+++ tuplas/indices.py
@@ -0,0 +1,13 @@
+cursos = ('PHP', 'Java', 'C#', 'JavaScript', 'Dart')
+
+primer_curso = cursos[0]
+print(primer_curso)
+
+ultimo_curso = cursos[-1]
+print(ultimo_curso)
+
+subtupla = cursos[0:2]
+print(subtupla)
+
+subtupla = cursos[::2]
+print(subtupla)
---
+++ tuplas/tupla.py
@@ -0,0 +1,6 @@
+# tuplas se pone en parentesis
+tupla = ()
+print(type(tupla))
+
+tupla = ('Noah', 24, -14.2, False, [1,2,4], (10,20,30,40,50))
+print(tupla)
