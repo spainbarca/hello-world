12d82f5af04b30cd976e31281eac69b7fed201d4
Noah <spain.barcelona.1999@gmail.com>
2023-11-20 23:04:30 -0500

    Multiples

---
+++ multiples.py
@@ -0,0 +1,13 @@
+# Asignar variables por cada linea
+"""
+nombre = 'Noah'
+apellido = 'Martinez'
+titulo = 'Ing.' 
+"""
+
+# Asignar variables en una sola linea (se separan en comas)
+nombre, apellido, titulo = 'Noah', 'Martinez', 'Ing.'
+
+print(nombre)
+print(apellido)
+print(titulo)
