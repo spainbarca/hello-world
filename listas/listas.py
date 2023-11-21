fc21b0621c4a97a62c8e6d648058f1102ca665c3
Noah <spain.barcelona.1999@gmail.com>
2023-11-21 00:17:15 -0500

    Rango de indice de una lista

---
+++ listas/listas.py
@@ -0,0 +1,14 @@
+# Basta con colocar la funcion list() o corchetes
+# Los´índices en python comienzan en 0
+
+#lista = list(1,3.14,'Perucito',False)
+lista = [1,3.14,'Perucito',False]
+
+lista_strings = ['Juan', 'Rojas','Sky']
+lista_cursos = ['Python', 'Flask','SQL','Laravel']
+lista_enteros = [14,12,34,-1]
+lista_floats = [14.2, 12.25,144.6]
+lista_booleanos = [True, False, (1 > 1)]
+
+#print(lista_cursos[3])
+print(lista_cursos[-1])
