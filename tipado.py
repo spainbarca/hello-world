4104233dfcd2c81fbd28dc39d5e17d237227953c
Noah <spain.barcelona.1999@gmail.com>
2023-11-20 22:17:10 -0500

    Tipado dinamico

---
+++ tipado.py
@@ -0,0 +1,13 @@
+# Las variables pueden almacenar diferentes tipos de datos en diferentes momentos
+
+valor = "Noah"
+print(valor)
+
+valor = 2
+print(valor)
+
+valor = 10.2
+print(valor)
+
+valor = True
+print(valor)
