fdf1f07bbf40aa1a8b60e47d29e94056f82d6e4d
Noah <spain.barcelona.1999@gmail.com>
2023-11-20 21:58:26 -0500

    Variables

---
+++ variables.py
@@ -0,0 +1,10 @@
+# <nombre> = <valor>
+
+titulo_curso = 'Introducción a Python'
+titulo_curso = 'Ahora es curso de PHP'
+
+nombre_completo='Introducción al curso de Python 3.11'
+
+#Palabras reservadas no usar
+
+print(nombre_completo)
