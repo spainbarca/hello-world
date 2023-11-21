20221751f024144a887ef576b14530043b848d8e
Noah <spain.barcelona.1999@gmail.com>
2023-11-21 18:01:39 -0500

    Operaciones con listas

---
+++ listas/operacioneslistas.py
@@ -0,0 +1,19 @@
+lista = [8,14,23,11,9,1,12,61,37]
+
+# .sort para ordenar la lista de forma ascendente
+lista.sort()
+print(lista)
+
+# .reverse para ordenar la lista de forma descendente
+lista.reverse()
+print(lista)
+
+# .min para sacar menor elemento de una lista
+print(min(lista))
+
+# .max para mayor elemento de una lista
+print(max(lista))
+
+# .in para saber si un elemento esta dentro del listado
+print(94 in lista)
+print(64 not in lista)
