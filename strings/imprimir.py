589f47527be1b98af93e74db31b1f4d6629239eb
Noah <spain.barcelona.1999@gmail.com>
2023-11-22 00:42:45 -0500

    Print

---
+++ strings/imprimir.py
@@ -0,0 +1,8 @@
+nombre = 'Noah'
+apellido = 'Martinez'
+
+# imprimir variables múltiples
+print(nombre, apellido, "Hancco", (14,12,4))
+
+# imprimir separando con otros caracteres
+print(nombre, apellido, "Hancco", (14,12,4), sep='@')
