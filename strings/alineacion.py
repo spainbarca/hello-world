5897301ecc9022f213bc0a46cef33e8ad063f138
Noah <spain.barcelona.1999@gmail.com>
2023-11-22 00:56:07 -0500

    Alineacion

---
+++ strings/alineacion.py
@@ -0,0 +1,10 @@
+mensaje = 'Hola mundo!!'
+
+# ljust -> Justifica a la Izquierda
+mensaje = mensaje.ljust(20)
+print(mensaje + '.')
+
+# rjust -> Justifica a la Derecha
+
+
+# center -> Centrar el texto
