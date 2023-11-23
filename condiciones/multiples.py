3e8590c9270a5b87643517b229a342a15c6fd775
Noah <spain.barcelona.1999@gmail.com>
2023-11-23 01:11:16 -0500

    Elif

---
+++ condiciones/multiples.py
@@ -0,0 +1,10 @@
+calificacion = 7
+
+if calificacion == 20 : 
+    print('Felicidades!! Aprobaste')
+elif calificacion == 8 or calificacion == 9:
+    print('Apenas aprobaste')
+elif calificacion == 7:
+    print('Pasaste')
+else:
+    print('Reprobaste el curso')
