8c132375170717539ca80c1a0ed3792b217d7bd3
Noah <spain.barcelona.1999@gmail.com>
2023-11-22 23:02:19 -0500

    Objetos de las llaves

---
+++ diccionarios/llaves.py
@@ -0,0 +1,15 @@
+diccionario = {'a':1,'b':2,'c':3,'d':4}
+
+# .keys
+llaves = diccionario.keys()
+print(llaves)
+
+# .values
+valores = diccionario.values()
+print(valores)
+
+# -items
+elementos = diccionario.items()
+print(elementos)
+
+#Convertir los objetos a tuplas
