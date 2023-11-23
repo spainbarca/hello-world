166a5b4a664a0d964ad2baec135e10e8e31250f1
Noah <spain.barcelona.1999@gmail.com>
2023-11-23 10:12:58 -0500

    operadores logicos

---
+++ condiciones/logicos.py
@@ -0,0 +1,18 @@
+variable = 'Cody' or 'CodigoFacilito'
+print(variable)
+
+variable = '' or 'CodigoFacilito'
+print(variable)
+
+variable = '' or 0 or [] or True
+print(variable)
+
+listado = []
+nombre='Noah'
+
+if listado:
+    variable = listado
+else:
+    variable = nombre
+
+print(variable)
