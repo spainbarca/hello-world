7e9a4d092a4fc838a64c41aa8d63e3e8fd2c4f4e
Noah <spain.barcelona.1999@gmail.com>
2023-11-23 01:23:58 -0500

    Ternario

---
+++ condiciones/ternario.py
@@ -0,0 +1,12 @@
+calificacion = 10
+color = None
+
+if calificacion>=7:
+    color = 'Blue'
+else: color = 'Red'
+
+print(calificacion, color)
+
+
+color = 'Verde' if calificacion >=7 else 'Rojo'
+print(calificacion, color)
