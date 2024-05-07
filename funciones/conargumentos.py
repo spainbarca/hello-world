cb112df6cc2484efea1b33cb1f9f19087db9a72a
Noah <practicasjaguar@gmail.com>
2024-05-07 00:39:14 -0500

    funciones con argumentos

---
+++ funciones/conargumentos.py
@@ -0,0 +1,8 @@
+def suma(a,b):
+    resultado= a+b
+    print(resultado)
+
+n1 = int(input('Ingresa el primer número entero: ')) #string
+n2 = int(input('Ingresa el segundo número entero: '))
+
+suma(n1,n2)
