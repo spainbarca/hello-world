ec031eb91dbb7e022e810e3b5d15b112219f9620
Noah <practicasjaguar@gmail.com>
2024-05-07 00:31:48 -0500

    intro a funciones sin argumento

---
+++ funciones/funciones.py
@@ -0,0 +1,8 @@
+def suma():
+    n1 = int(input('Ingresa el primer número entero: ')) #string
+    n2 = int(input('Ingresa el segundo número entero: '))
+
+    resultado=n1+n2
+    print(resultado)
+
+suma()
