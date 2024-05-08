95107a692b388a6f5549d9ec8f71b700755cfbb1
Noah <practicasjaguar@gmail.com>
2024-05-07 20:21:52 -0500

    argumentos parte 2

--- funciones/args.py
+++ funciones/args.py
@@ -3,5 +3,14 @@ def promedio(*args):
     print(type(args))
     return sum(args)/len(args)
 
+
+def combinacion(p1, p2, *args, p4):
+    print(p1)
+    print(p2)
+    print(args)
+    print(p4)
+
 resul_prom = promedio(14, 20, 12, 16, 13)
 print(resul_prom)
+
+combinacion(10,25,14,12,14,58,p4=100)
