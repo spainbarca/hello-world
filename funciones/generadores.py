737da4f28d91b53cfeeb3708276ee1ee470414d7
Noah <practicasjaguar@gmail.com>
2024-05-08 02:17:47 -0500

    distribucion peresoza

--- funciones/generadores.py
+++ funciones/generadores.py
@@ -1,6 +1,14 @@
 def pares(): # -> Lazy itirator
-    for numero in range(0,100,2):
+    for numero in range(0,16,2):
         yield numero # La funcion suspende su ejecucion
+        print ("Se reanuda la ejecucion")
 
-for par in pares():
-    print(pares())
+generador = pares()
+
+while True:
+    try:
+        par = next(generador)
+        print(par)
+    except StopIteration:
+        print("el generador finalizó")
+        break
