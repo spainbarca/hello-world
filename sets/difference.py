61deaeac6e587b0306aef67893adf4bb19e8b097
Noah <practicasjaguar@gmail.com>
2024-05-12 01:48:55 -0500

    difference

---
+++ sets/difference.py
@@ -0,0 +1,11 @@
+lista5 = {"PHP","Java","Python","SQL","C#"}
+lista6 = {"Java","C#","C++"}
+
+print(lista5)
+print(lista6)
+print()
+
+todo_lista3=lista5.difference(lista6)
+print(todo_lista3)
+todo_lista4=lista6.difference(lista5)
+print(todo_lista4)
