05182a390a418bae1846a6f388c35bd6076f5e7b
Noah <practicasjaguar@gmail.com>
2024-05-12 01:00:00 -0500

    suma promedio listas

---
+++ listas/sum_prom.py
@@ -0,0 +1,19 @@
+#Sumatoria
+lista1 = [1,3,5,10,47,26]
+sumatoria = 0
+
+for num in lista1:
+    sumatoria+=num
+
+print(sumatoria)
+
+
+#Promedio
+lista2 = [15,26,75,20,67,14]
+promedio = 0
+
+for num in lista2:
+    promedio+=num
+
+promedio/=len(lista2)
+print(promedio)
